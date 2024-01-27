2fba74c06bf84aea47579e5d69ce9e290534accb
ComittedComitter <65293328+ComittedComitter@users.noreply.github.com>
2024-01-27 15:18:34 +0100

    Start on couch collistion

--- Scripts/cat.gd
+++ Scripts/cat.gd
@@ -7,5 +7,12 @@ const speed = 300.0
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-
 	move_and_slide()
+	if Input.is_action_pressed("wreck_button"):
+		var collision_body = get_collider()
+		if collision_body:
+			if collision_body.has_method("wreck"):
+				collision_body.wreck()
+			
+
+
